2ef0d55f7b1102d1b2cf924734e511ab87136383
tushar soni <soni.tushar93@gmail.com>
2016-08-19 23:53:22 +0530

    changes

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-
 
 var fs = require('fs');
 var express = require('express');
@@ -21,28 +19,3 @@ io.sockets.on('connection', function(client){
     });
 
 });
-||||||| merged common ancestors
-=======
-
-
-var fs = require('fs');
-var express = require('express');
-var app = express();
-var server = app.listen(8080,'0.0.0.0');
-var io = require('socket.io').listen(server);
-
-// index.html
-app.get('/', function(req, res) {
-    fs.createReadStream('index.html').pipe(res);
-});
-app.disable('x-powered-by');
-// sockets
-io.sockets.on('connection', function(client){
-
-    // when a message comes in from a client, re-emit it from the server to client(s)
-    client.on('message:client', function(data) {
-        client.broadcast.emit('message:server', {message: data.message, transdata: data.transdata});
-    });
-
-});
->>>>>>> 72e858e083ffd9909c7e9ba393edf60d04f09929
