07f1e5c63025ab04454ea4ffa3ed37c87ceef314
Tushar <soni.tushar93@gmail.com>
2016-08-19 20:17:13 +0530

    temp changes

--- app.js
+++ app.js
@@ -3,10 +3,10 @@
 var fs = require('fs');
 var express = require('express');
 var app = express();
-var server = app.listen(process.env.PORT);
+var server = app.listen(8080, 'localhost');//app.listen(process.env.PORT);
 var io = require('socket.io').listen(server);
 
-// index.html 
+// index.html
 app.get('/', function(req, res) {
     fs.createReadStream('index.html').pipe(res);
 });
@@ -14,7 +14,7 @@ app.get('/', function(req, res) {
 // sockets
 io.sockets.on('connection', function(client){
 
-    
+
     client.on('message:client', function(data) {
         client.broadcast.emit('message:server', {message: data.message, transdata: data.transdata});
     });
